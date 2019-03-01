7d4599ecdeec1d104c5b97190795311c5a7d8eac
Mary-N <marynakanjako@gmail.com>
2019-03-01 18:25:17 +0300

    I didn't change much in this commit

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,17 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from django.http import Http404
 from .models import Question
 
 # Create your views here.
+
+    # A basic index view
 #def index(request):
 #    return HttpResponse("Hello, world. You're at the polls index.")
+
+    # writing index view with parameters
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     output = ', '.join([q.question_text for q in latest_question_list])
+#     return HttpResponse(output)
+
+#rewriting the index view using the render() function
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
+    # A basic detail view
+# def detail(request, question_id):
+#     return HttpResponse("You're looking at question %s." % question_id)
+
+    #using try except
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#         context = {'question': question}
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request, 'polls/detail.html', context)
+
+    # using get_object_or_404 instead of try except
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
